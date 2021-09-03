972b25d54a2b4c27a26d1a5866b334083ea40a5d
hu177 <767930747@qq.com>
2021-09-03 20:20:40 +0800

    asdf

--- pkg/storage/hdfs.go
+++ pkg/storage/hdfs.go
@@ -2,21 +2,21 @@ package storage
 
 import (
 	"context"
-	"encoding/xml"
 	"fmt"
 	"github.com/colinmarc/hdfs/v2"
 	"github.com/colinmarc/hdfs/v2/hadoopconf"
+	krb "github.com/jcmturner/gokrb5/v8/client"
+	"github.com/jcmturner/gokrb5/v8/config"
+	"github.com/jcmturner/gokrb5/v8/credentials"
 	"github.com/pingcap/errors"
 	"github.com/pingcap/log"
-	krb "gopkg.in/jcmturner/gokrb5.v7/client"
-	"gopkg.in/jcmturner/gokrb5.v7/config"
-	"gopkg.in/jcmturner/gokrb5.v7/credentials"
 	"io/fs"
-	"io/ioutil"
+	"net"
 	"os"
 	"os/user"
 	"path/filepath"
 	"strings"
+	"time"
 )
 
 // TODO: 在csv写入通路中加入该writer
@@ -151,34 +151,6 @@ type HdfsConfig struct {
 	HdfsSiteConf string
 }
 
-type property struct {
-	Name  string `xml:"name"`
-	Value string `xml:"value"`
-}
-
-type propertyList struct {
-	Property []property `xml:"property"`
-}
-
-func ClientOptionsFromConf(conf hadoopconf.HadoopConf) hdfs.ClientOptions {
-	options := hdfs.ClientOptions{Addresses: conf.Namenodes()}
-
-	options.UseDatanodeHostname = (conf["dfs.client.use.datanode.hostname"] == "true")
-
-	if strings.ToLower(conf["hadoop.security.authentication"]) == "kerberos" {
-		// Set an empty KerberosClient here so that the user is forced to either
-		// unset it (disabling kerberos altogether) or replace it with a valid
-		// client. If the user does neither, NewClient will return an error.
-		options.KerberosClient = &krb.Client{}
-	}
-
-	if conf["dfs.namenode.kerberos.principal"] != "" {
-		options.KerberosServicePrincipleName = strings.Split(conf["dfs.namenode.kerberos.principal"], "@")[0]
-	}
-
-	return options
-}
-
 func getKerberosClient() (*krb.Client, error) {
 	configPath := os.Getenv("KRB5_CONFIG")
 	if configPath == "" {
@@ -213,59 +185,12 @@ func getKerberosClient() (*krb.Client, error) {
 		return nil, err
 	}
 
-	client, err := krb.NewClientFromCCache(ccache, cfg)
+	client, err := krb.NewFromCCache(ccache, cfg)
 	if err != nil {
 		return nil, err
 	}
 
 	return client, nil
-}
-
-func newHdfsClientWithPath() (*hdfs.Client, error) {
-	conf, err := hadoopconf.Load("/usr/local/bin/")
-	options := hdfs.ClientOptionsFromConf(conf)
-
-	u, err := user.Current()
-	if err != nil {
-		return nil, err
-	}
-
-	options.User = u.Username
-	if options.KerberosClient != nil {
-		options.KerberosClient, err = getKerberosClient()
-		if err != nil {
-			return nil, errors.Wrap(err, "getKerberosClient failure")
-		}
-	}
-
-	return hdfs.NewClient(options)
-}
-
-func loadWithPaths(paths []string) (hadoopconf.HadoopConf, error) {
-	fmt.Printf("Loading WithPaths：%v\n", paths)
-	var conf hadoopconf.HadoopConf
-	for _, file := range paths {
-		pList := propertyList{}
-		f, err := ioutil.ReadFile(file)
-		if os.IsNotExist(err) {
-			continue
-		} else if err != nil {
-			return conf, err
-		}
-
-		if conf == nil {
-			conf = make(hadoopconf.HadoopConf)
-		}
-
-		err = xml.Unmarshal(f, &pList)
-		if err != nil {
-			return conf, fmt.Errorf("%s: %s", file, err)
-		}
-		for _, prop := range pList.Property {
-			conf[prop.Name] = prop.Value
-		}
-	}
-	return conf, nil
 }
 
 func (s *HdfsStorage) dirExist(dirPath string) (bool, error) {
@@ -286,19 +211,12 @@ func (s *HdfsStorage) GetBaseDir() string {
 
 func newHdfsStorage(ctx context.Context, bdh *HdfsConfig, opts *ExternalStorageOptions) (*HdfsStorage, error) {
 	// 从命令行配置中读取配置文件
-	client, err := newHdfsClientWithPath()
+	//client, err := newHdfsClientWithPath()
+	client, err := NewHdfsClient()
 	if err != nil {
 		return nil, errors.Wrap(err, "newHdfsStorage error")
 	}
-	//clientConf, err := hadoopconf.Load("/usr/local/bin/")
-	//if err != nil {
-	//	return nil, errors.WithStack(err)
-	//}
-	//clientOpts := hdfs.ClientOptionsFromConf(clientConf)
-	//client, err := hdfs.NewClient(clientOpts)
-	//if err != nil {
-	//	return nil, errors.WithStack(err)
-	//}
+
 	retStorage := &HdfsStorage{client: client}
 	retStorage.client = client
 	// 先检查该文件夹是否存在，存在需要清除文件夹内容
@@ -337,10 +255,59 @@ func newHdfsStorage(ctx context.Context, bdh *HdfsConfig, opts *ExternalStorageO
 func (s *HdfsStorage) ReConnect() (*HdfsStorage, error) {
 	var retStorage HdfsStorage
 	retStorage.base = s.base
-	client, err := newHdfsClientWithPath()
+	client, err := NewHdfsClient()
 	if err != nil {
 		return s, errors.Wrap(err, "Create client failure")
 	}
 	retStorage.client = client
 	return &retStorage, nil
+}
+
+// 获取新的Client链接
+func NewHdfsClient() (*hdfs.Client, error) {
+	namenode := os.Getenv("HADOOP_NAMENODE")
+	conf, err := hadoopconf.LoadFromEnvironment()
+	if err != nil {
+		return nil, fmt.Errorf("Problem loading configuration: %s", err)
+	}
+	options := hdfs.ClientOptionsFromConf(conf)
+	if namenode != "" {
+		options.Addresses = []string{namenode}
+	}
+
+	if options.Addresses == nil {
+		return nil, errors.New("Couldn't find a namenode to connect to. You should specify hdfs://<namenode>:<port> in your paths. Alternatively, set HADOOP_NAMENODE or HADOOP_CONF_DIR in your environment.")
+	}
+
+	if options.KerberosClient != nil {
+		options.KerberosClient, err = getKerberosClient()
+		if err != nil {
+			return nil, fmt.Errorf("Problem with kerberos authentication: %s", err)
+		}
+	} else {
+		options.User = os.Getenv("HADOOP_USER_NAME")
+		if options.User == "" {
+			u, err := user.Current()
+			if err != nil {
+				return nil, fmt.Errorf("Couldn't determine user: %s", err)
+			}
+
+			options.User = u.Username
+		}
+	}
+
+	dialFunc := (&net.Dialer{
+		Timeout:   5 * time.Second,
+		KeepAlive: 5 * time.Second,
+		DualStack: true,
+	}).DialContext
+
+	options.NamenodeDialFunc = dialFunc
+	options.DatanodeDialFunc = dialFunc
+
+	c, err := hdfs.NewClient(options)
+	if err != nil {
+		return nil, fmt.Errorf("Couldn't connect to namenode: %s", err)
+	}
+	return c, nil
 }
